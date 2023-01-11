15fdda1632321cc03e60ae0ccf16be1068195c43
Snoopy <kentincosnard@gmail.com>
2023-01-11 19:05:07 +0100

    setup ProductService

---
+++ src/Service/Core/IProductService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service\Core;
+
+use App\Entity\Product;
+
+interface IProductService
+{
+    public function getAll(): array;
+    public function create(array $raw): Product;
+    public function read(string $id): Product;
+    public function update(string $id, array $raw): Product;
+    public function delete(string $id): Product;
+}

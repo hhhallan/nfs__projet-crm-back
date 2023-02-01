b01e65ceb2ad9a40674e0e3af2bc34698ed91d70
m02xime <maxkb02@gmail.com>
2023-02-01 09:57:05 +0100

    fait un commit

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Service\FactureService;
 use Stripe;
 
 #[Route('/api')]
@@ -22,28 +23,35 @@ class StripeController extends AbstractController
     }
 
 
-    #[Route('/stripe/create-token', name: 'app_stripe_token', methods: ['POST'])]
-    public function createToken(Request $request)
+    #[Route('/stripe/create-payment/{id}', name: 'app_stripe_payment', methods: ['POST'])]
+    public function createPayment(Request $request, $id, FactureService $factureService)
     {
+        $invoice = $factureService->read($id);
+
+        if (!$invoice) {
+            return new JsonResponse([
+                'error' => 'No such invoice.'
+            ], 400);
+        }
+
+        if ($invoice->getStat() === 'PAYED') {
+            return new JsonResponse([
+                'error' => 'Invoice already paid.'
+            ], 400);
+        }
+
         Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
 
-        $token = Stripe\Token::create([
-            "card" => [
-                "number" => $request->request->get('card_number'),
-                "exp_month" => $request->request->get('exp_month'),
-                "exp_year" => $request->request->get('exp_year'),
-                "cvc" => $request->request->get('cvc'),
-            ],
-        ]);
+        $card = [
+            "number" => $request->request->get('card_number'),
+            "exp_month" => $request->request->get('exp_month'),
+            "exp_year" => $request->request->get('exp_year'),
+            "cvc" => $request->request->get('cvc'),
+        ];
 
-        return new JsonResponse(['token' => $token['id']]);
-    }
+        $token = Stripe\Token::create(["card" => $card]);
+        $stripeToken = $token['id'];
 
-
-    #[Route('/stripe/create-charge', name: 'app_stripe_charge', methods: ['POST'])]
-    public function createCharge(Request $request)
-    {
-        $stripeToken = $request->request->get('stripeToken');
         $amount = $request->request->get('amount');
         $description = $request->request->get('description');
 
@@ -53,13 +61,20 @@ class StripeController extends AbstractController
             ], 400);
         }
 
-        Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
-        Stripe\Charge::create([
-            "amount" => $amount,
-            "currency" => "eur",
-            "source" => $stripeToken,
-            "description" => $description
-        ]);
+        try {
+            Stripe\Charge::create([
+                "amount" => $amount,
+                "currency" => "eur",
+                "source" => $stripeToken,
+                "description" => $description
+            ]);
+        } catch (\Exception $e) {
+            return new JsonResponse([
+                'error' => 'Payment failed.'
+            ], 400);
+        }
+
+        $factureService->changeState($id, 'PAYED');
 
         return new JsonResponse([
             'message' => 'Payment Successful!'
